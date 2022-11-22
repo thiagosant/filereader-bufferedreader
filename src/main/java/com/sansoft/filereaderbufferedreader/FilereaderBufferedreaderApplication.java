01c6214776d2dd2c6f2f2411b359e0f3c45f7fab
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2022-11-21 23:21:42 -0300

    Class 215 - FileReader and BufferedReader

---
+++ src/main/java/com/sansoft/filereaderbufferedreader/FilereaderBufferedreaderApplication.java
@@ -0,0 +1,45 @@
+package com.sansoft.filereaderbufferedreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+@SpringBootApplication
+public class FilereaderBufferedreaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilereaderBufferedreaderApplication.class, args);
+
+        String path = "C:\\temp\\ina.txt";
+
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            try {
+                if (br != null) br.close();
+                if (fr != null) fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/sansoft/filereaderbufferedreader/FilereaderBufferedreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansoft.filereaderbufferedreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilereaderBufferedreaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
